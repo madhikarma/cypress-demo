8b544b159ddc88ee14fb79a7c6a96d1777baef8a
Shagun Madhikarmi <madhikarma@gmail.com>
2023-01-15 11:24:16 +0000

    Fix should vs contains + Adds code comments  to cypress e2e tests

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -7,51 +7,67 @@ import {
 describe("The App", () => {
   // Test 1
   it("loads successfully", () => {
-    // Load web page into browser i.e. launch web app
+    // Given
+
+    // - Load web page into browser i.e. launch web app
     cy.visit("http://localhost:3000");
   });
 
   // Test 2
   it("contains welcome text", () => {
     // Given
-    // Load web page into browser i.e. launch web app
+
+    // - Load web page into browser i.e. launch web app
     cy.visit("http://localhost:3000");
 
-    // Find <p> label and assert the text
+    // When / Then
+
+    // - Find <p> label and assert the text
     cy.contains("Edit src/App.js and save to reload.");
   });
 
   // Test 3
   it("contains show alert button", () => {
-    // Load web page into browser i.e. launch web app
+    // Given
+
+    // - Load web page into browser i.e. launch web app
     cy.visit("http://localhost:3000");
 
-    // Find <button> and assert the text
+    // When
+
+    // - Find <button> and assert the text
     cy.contains("button", "Show alert");
 
-    // Find <button> and click it
+    // - Find <button> and click it
     // cy.get('button').click()
-    // Use ids as below (see src/App.js to edit the id)
+
+    // - Use ids as below (see src/App.js to edit the id)
     // cy.get('[data-cy="show-alert-button"]').click()
 
-    // Assert alert popup
+    // Then
+
+    // - Assert alert popup
     cy.on("window:alert", (text) => {
-      //Mocha assertions
       expect(text).to.contains("Hello world");
     });
   });
 
   // Test 4
   it("shows alert when show alert button is clicked", () => {
-    // Load web page into browser i.e. launch web app
+    // Given
+
+    // - Load web page into browser i.e. launch web app
     cy.visit("http://localhost:3000");
 
-    // Find <button> and click it
+    // When
+
+    // - Find <button> and click it
     cy.get('[data-cy="show-alert-button"]').click();
 
-    // Assert alert popup
+    // Then
+
+    // - Assert alert popup
     cy.on("window:alert", (text) => {
-      //Mocha assertions
       expect(text).to.contains("Hello world");
     });
   });
@@ -59,15 +75,19 @@ describe("The App", () => {
   // Test 5
   /*
   it("load new website when button is clicked", () => {
-    // Load web page into browser i.e. launch web app
+    // Given
+
+    // - Load web page into browser i.e. launch web app
     cy.visit("http://localhost:3000");
 
-    // Find <button> and click it
+    // When
+
+    // - Find <button> and click it
     cy.get('[data-cy="load-new-website-button"]').click();
 
-    // wait(10);
+    // Then
 
-    // Assert
+    // - Check URL location
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq("https://www.google.com");
     });
@@ -76,22 +96,40 @@ describe("The App", () => {
 
   // Test 6
   it("show login form username label", () => {
-    // Load web page into browser i.e. launch web app
+    // Given
+
+    // - Load web page into browser i.e. launch web app
     cy.visit("http://localhost:3000");
 
-    // Find login form username label
+    // When
+
+    // - 1. Find by id
     var usernameLabel = cy.get('[data-cy="login-form-username-label"]');
-    usernameLabel.contains("Username:");
+    // - 2. Find by HTML tag lookup
+    // var usernameLabel = cy.contains("label", "Username:")
+
+    // Then
+    usernameLabel.should("have.text", "Username:");
   });
 
   // Test 7
   it("show login form password label", () => {
+    // Given:
+
     // Load web page into browser i.e. launch web app
     cy.visit("http://localhost:3000");
 
-    // Find login form password label
+    // When
+
+    // - Find
+
+    // - 1. Find by id
     var passwordLabel = cy.get('[data-cy="login-form-password-label"]');
-    passwordLabel.contains("Password:");
+    // - 2. Find by HTML tag lookup
+    // var passwordLabel = cy.contains("label", "Password:")
+
+    // Then
+    passwordLabel.should("have.text", "Password:");
   });
 
   // Test 8
@@ -99,22 +137,34 @@ describe("The App", () => {
     // Load web page into browser i.e. launch web app
     cy.visit("http://localhost:3000");
 
-    // Find login form submit button
+    // When
+
+    // - 1. Find by id
     var submitButton = cy.get('[data-cy="login-form-submit-button"]');
-    submitButton.contains("Submit");
+    // - 2. Find by HTML tag lookup
+    // var submitButton = cy.contains("Submit");
+
+    // Then
+    submitButton.should("have.text", "Submit:");
   });
 
   // Test 9
   it("show alert popup when login form submit button is pressed", () => {
-    // Load web page into browser i.e. launch web app
+    // Given
+
+    // - Load web page into browser i.e. launch web app
     cy.visit("http://localhost:3000");
 
-    // Find login form submit button
+    // When
+
+    // - Find login form submit button
     var submitButton = cy.get('[data-cy="login-form-submit-button"]');
     submitButton.contains("Submit");
     submitButton.click();
 
-    // Assert alert popup
+    // Then
+
+    // - Assert alert popup
     cy.on("window:alert", (text) => {
       expect(text).to.contains("Login sent!");
       expect(text).to.contains("Username:"); // Note. will be 'Username: undefined' until the app uses validation
