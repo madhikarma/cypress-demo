615a5ab54982b0f014dd576e5c41b64ab1d70744
Shagun Madhikarmi <madhikarma@gmail.com>
2023-03-12 07:40:05 +0000

    Add more cypress e2e tests for product card

--- cypress/e2e/productCard.cy.js
+++ cypress/e2e/productCard.cy.js
@@ -1,13 +1,40 @@
-describe("The App", () => {
-    it("should render product card", () => {
-        // Given
-        cy.visit("http://localhost:3000");
+describe("The Product Card", () => {
+  it("should render product card", () => {
+    // Given
+    cy.visit("http://localhost:3000");
 
-        // When
-        var image = cy.get('img')
-        
-        // Then
-        image.should('be.visible')
-        image.should('have.attr', 'src', '/static/media/Jordanlow2_1400x.0e81af5d0765dd76fda4.jpg')
-    });
+    // When
+    var productCard = cy.get(".productCard");
+
+    // Then
+    productCard.should("be.visible");
+  });
+
+  it("should render product card image", () => {
+    // Given
+    cy.visit("http://localhost:3000");
+
+    // When
+    var image = cy.get("img");
+
+    // Then
+    image.should("be.visible");
+    image.should(
+      "have.attr",
+      "src",
+      "/static/media/Jordanlow2_1400x.0e81af5d0765dd76fda4.jpg"
+    );
+  });
+
+  it("should render product card name", () => {
+    // Given
+    cy.visit("http://localhost:3000");
+
+    // When
+    var name = cy.get(".productCardName");
+
+    // Then
+    name.should("be.visible");
+    name.should("have.text", "Nike Air Jordans");
+  });
 });
--- src/ProductCard.js
+++ src/ProductCard.js
@@ -18,7 +18,7 @@ class ProductCard extends React.Component {
 
   renderIsOnSaleLabel() {
     if (this.isOnSale()) {
-      return <p>SALE</p>
+      return <p className='productCardSalePrice'>SALE</p>
     } 
     return null
   }
@@ -26,17 +26,17 @@ class ProductCard extends React.Component {
   // Functions (React framework render function)
   render() {
     return (
-      <div className="productCard">
-        <a href={this.state.productURL}>
-          <img src={this.state.image} width="200" height="200" />
+      <div className="productCard" data-cy="product-card">
+        <a href={this.state.productURL} className='productCardImageLink' data-cy="product-card-image-link">
+          <img src={this.state.image} width="200" height="200" className='productCardImage' data-cy="product-card-image" />
         </a>
-        <p>{this.state.name}</p>
-        <div className="priceContainer">
-          <p className={ this.isOnSale() ? 'oldPrice' : 'regularPrice'}>{this.state.price}</p>
-          <p className='salePrice'>{this.state.salePrice}</p>
+        <p className='productCardName' data-cy="product-card-name">{this.state.name}</p>
+        <div className="priceContainer" data-cy="product-card-price-container">
+          <p className={ this.isOnSale() ? 'productCardOldPrice' : 'productCardRegularPrice'} data-cy="product-card-price">{this.state.price}</p>
+          <p className='salePrice' data-cy="product-card-sale-price">{this.state.salePrice}</p>
         </div>
         {this.renderIsOnSaleLabel()}
-        <p>{this.state.description}</p>
+        <p className='productCardDescription' data-cy="product-card-description">{this.state.description}</p>
       </div>
     )
   }
