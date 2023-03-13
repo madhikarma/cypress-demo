6fa87d9d8bc4d9e0a62e59c9174299ec12d5cc02
Shagun Madhikarmi <madhikarma@gmail.com>
2023-03-13 09:45:11 +0000

    Rename images

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import ProductCard from './ProductCard';
-import nikeJordansSneakerImage from './images/Jordanlow2_1400x.jpg';
-import nikeZoomVistaGrindImage from './images/Nike-Zoom-X-Vista-Grind-Sneaker.jpg';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ProductCard from "./ProductCard";
+import image from "./images/shoes/nike-air-jordan.jpg";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* Note. UNCOMMENT BELOW TO RENDER / LOAD THE APP/LOGIN FORM UI */}
     {/* <App value=''/> */}
-    <ProductCard productURL="https://www.nike.com" image={nikeJordansSneakerImage} name="Nike Air Jordans" description="Some description about these Nike Jordan sneakers." price="£39.99" salePrice="£35.99"/>
-    {/* <ProductCard productURL="https://www.nike.com" image={nikeZoomVistaGrindImage} name="Nike Zoom Vista" description="Some description about these Nike Zoom Vista Grind sneakers." price="£69.99" /> */}
+
+    {/* Note. UNCOMMENT BELOW TO RENDER / LOAD THE PRODUCT CARD UI */}
+    <ProductCard
+      productURL="https://www.nike.com"
+      image={image}
+      name="Nike Air Jordans"
+      description="Some description about these Nike Jordan sneakers."
+      price="£39.99"
+      salePrice="£35.99"
+    />
   </React.StrictMode>
 );
 
